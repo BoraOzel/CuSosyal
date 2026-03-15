e9688fc8013554c598010517e42b1e95b9f7d663
BoraOzel <bboraozell@gmail.com>
2026-03-15 11:41:34 +0300

    feat: add Router for auth based initial screen navigation.

--- CuSosyal/CuSosyal/Application/SceneDelegate.swift
+++ CuSosyal/CuSosyal/Application/SceneDelegate.swift
@@ -18,8 +18,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = MainTabBarController()
         self.window = window
+        Router.presentInitialScreen(window: window)
         window.makeKeyAndVisible()
     }
 
---
+++ CuSosyal/CuSosyal/Utilities/Router.swift
@@ -0,0 +1,30 @@
+//
+//  Router.swift
+//  CuSosyal
+//
+//  Created by Bora Özel on 15/3/26.
+//
+
+import Foundation
+import UIKit
+
+class Router {
+    
+    static func presentInitialScreen(window: UIWindow) {
+        if AuthManager.shared.isSignedIn {
+            switchToApp(window: window)
+        }
+        else {
+            switchToAuth(window: window)
+        }
+    }
+    
+    static func switchToApp(window: UIWindow) {
+        window.rootViewController = MainTabBarController()
+    }
+    
+    static func switchToAuth(window: UIWindow) {
+        window.rootViewController = UINavigationController(rootViewController: LoginViewController())
+    }
+    
+}
